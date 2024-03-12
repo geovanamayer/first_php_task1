e793466847623cb022ecaea9559d4b795ac071ad
Geovana Nogueira Mayer <99758327+geovanamayer@users.noreply.github.com>
2024-03-12 20:03:28 -0300

    Add files via upload

---
+++ lang/app/Http/Controllers/AulaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AulaController extends Controller
+{
+    //
+    public function index(){
+        return view('exemplo');
+    }
+
+    public function update(){
+      return view('exemplo');
+    }
+
+    
+}
+
